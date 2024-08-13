dd8e1d21101c7369323ba6eb4a0ed39629cfaa8f
Jaime Bello <jaime10016@hotmail.com>
2024-08-13 15:20:47 -0500

    Project's birth

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Custom_AI_Coworker
+
+![Coworker AI](https://github.com/avechuch0/custom_ai_coworker/blob/main/images/logo.png)
+
+AI Coworker is an AI-powered assistant designed to help you with various tasks in a workspace environment. It utilizes OpenAI's language models to provide intelligent responses, helping you make informed decisions and streamline your workflow.
+
+This project is a RAG (Retrieval Augmented Generation) algorithm to assist in your particular needs and data not included in LLMs market for obvious reasons. 
+
+"LLMs are trained on enormous bodies of data but they aren't trained on your data", LlamaIndex.
+
+This is the general view of a RAG concept, courtesy of LlamaIndex.
+
+![RAG](https://github.com/avechuch0/custom_ai_coworker/blob/main/images/RAG.png)
+
+## Features
+
+- **Model Selection:** Choose from different GPT models, including `gpt-4o-mini`, `gpt-4o`, and `gpt-4-turbo`.
+- **Customizable Settings:** Adjust the temperature setting to control the creativity of the AI's responses.
+- **User-Friendly Interface:** Powered by Gradio for a simple and interactive experience.
+- **Storage Integration:** Save and load indexes with ease, ensuring persistent and retrievable data.
+- **Command-Line Options:** Customize your experience with command-line arguments for model selection and temperature control.
+
+## Installation
+
+1. Download / clone the repo
+2. Install required packages: ```pip3 install -r requirements.txt``` or ```pip install -r requirements.txt``` 
+3. Provide the OpenAI apikey next to the equal sign and within the quotes ```api_key = os.environ["OPENAI_API_KEY"] = 'your_apikey_here' ```
+
+## Usage
+
+**Command-Line Arguments**
+- **-title**: Set the title for your session, you can use your particular topic, process etc of your trained data. This is a required argument.
+- **-temp**: Set the temperature for the model’s responses. Acceptable range is 0.0 to 1.0.
+- After running it, you would be asked about the model to use, just select between 1 to 3.
+
+## Interface
+Once the program is running, you can interact with AI Coworker via a Gradio-powered web interface. Just enter your queries and receive intelligent responses.
+
+## Proof of Concept
+To illustrate, I used ChatGPT to create a couple of documents of a ficticious company called ACME Corp including some details of technology deployed, it was intentionally made with vulnerabilities and misconfigurations. I used this project with ACME docs to train this new model to get advise and recommendations provided by the prompts sent to the LLM in Gradio web interface. 
+
+There are many use cases of this project, it's up to you :)
+
+![POC1](https://github.com/avechuch0/custom_ai_coworker/blob/main/images/POC_1.png)
+
+![POC2](https://github.com/avechuch0/custom_ai_coworker/blob/main/images/POC_2.png)
+
+## Contact
+Twitter: [@avechuch0](https://twitter.com/avechuch0)
---
+++ aicoworker.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+import gradio as gr
+import os
+import argparse
+from colorama import Fore, Style, init
+from llama_index.llms.openai import OpenAI
+from llama_index.core import Settings
+from llama_index.core import (    
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,    
+    VectorStoreIndex,
+)
+
+# Provide your OpenAI API key
+api_key = os.environ["OPENAI_API_KEY"] = ''
+
+init()
+
+robot_ascii_art = f"""
+{Fore.RED}     _______              
+{Fore.RED}    /       \  Coworker AI - By avechuch0      
+{Fore.CYAN}   |{Fore.YELLOW}  O   O  {Fore.CYAN}|           
+{Fore.CYAN}   |{Fore.RED}    >    {Fore.CYAN}|           
+{Fore.CYAN}   |  {Fore.RED}\___/  {Fore.CYAN}|           
+{Fore.CYAN}   |_________|            
+{Fore.CYAN}    /  | |  \            
+{Fore.CYAN}   |___|_|___|           
+{Fore.CYAN}     |_| |_|
+{Style.RESET_ALL}"""
+
+def select_model():
+    print("Select the GPT model you want to use (ordered by cost from cheapest to most expensive):")
+    print("1. gpt-4o-mini")           
+    print("2. gpt-4o")                
+    print("3. gpt-4-turbo")           
+
+    choice = input("Enter the number corresponding to the model you want to choose: ")
+
+    if choice == "1":
+        return "gpt-4o-mini"
+    elif choice == "2":
+        return "gpt-4o"
+    elif choice == "3":
+        return "gpt-4-turbo"
+    else:
+        print("Invalid choice. Defaulting to gpt-4o-mini.")
+        return "gpt-4o-mini"
+
+def create_and_save_index(directory_path, model, temperature):
+    # check if storage already exists
+    PERSIST_DIR = directory_path
+    if not os.path.exists(PERSIST_DIR):
+        # Load the documents and create the index
+        documents = SimpleDirectoryReader("docs").load_data()
+        index = VectorStoreIndex.from_documents(documents)
+        # Store it for later
+        index.storage_context.persist(persist_dir=PERSIST_DIR)
+    else:
+        # Load the existing index
+        storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+        index = load_index_from_storage(storage_context)
+
+    Settings.llm = OpenAI(temperature=temperature, model=model)
+    return index
+
+def generate_response(input_text):
+    # Load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    index = load_index_from_storage(storage_context)    
+    query_engine = index.as_query_engine()
+    response = query_engine.query(input_text)
+    return response.response
+
+def launch_interface(title, model, temperature):
+    label_text = f"Ask the AI Coworker around {title}"
+    iface = gr.Interface(
+        fn = generate_response,
+        inputs = gr.components.Textbox(lines=4, label=label_text),
+        outputs = "text",
+        title = title,
+    )    
+    create_and_save_index("./storage", model, temperature)
+    iface.launch(share=True)
+
+if __name__ == "__main__":
+    print(robot_ascii_art)
+    parser = argparse.ArgumentParser(description="Use AI to train your data stored in the 'docs' folder")
+    parser.add_argument("-title", type=str, required=True, help="Title or topic for your AI coworker")
+    parser.add_argument("-temp", type=float, required=False, default=0.5, help="The temperature setting for the GPT model (e.g., 0.0 to 1.0)")
+    args = parser.parse_args()
+
+    # Call the select_model function to get the desired model
+    selected_model = select_model()
+
+    # Launch the interface with the selected title, model, and temperature
+    launch_interface(args.title, selected_model, args.temp)
